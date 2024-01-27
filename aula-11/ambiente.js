31a9d1792c822039ac2fd0566015e5b1ae2e8537
Moisés Barros <moisesbarrosdev@gmail.com>
2024-01-27 20:02:03 -0300

    moore class

---
+++ aula-11/ambiente.js
@@ -0,0 +1,17 @@
+let contador = 1
+// while = se ler assim : enquanto...
+while (contador <= 6 ) {
+  console.log (`Passo ${contador}`)  // o codigo se ler assim ;  enquanto a variavel contador for menor que 6, ele vai ficar executando o console.log(`Passo ${contador}`) até ele ficar maior ou igual a 6.
+  contador++  // isso sgnfica contador = contador + 1     
+                    //  aperte ctrl ç  e veja oque acontece
+}
+
+
+
+let c = 1
+// do = se ler assim : faça...
+do  {
+  console.log (`Passo ${c}`)  
+  c++  // isso sgnfica c = c + 1    
+  //  aperte ctrl ç  e veja oque acontece
+} while (c <= 6 )
